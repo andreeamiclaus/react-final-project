1acb7bafa2a86240ce71c3b4ee69cfb23d77dee2
andreeamiclaus <amiclaus.a@gmail.com>
2022-11-09 22:11:30 +0100

    Added github url

--- src/App.js
+++ src/App.js
@@ -1,5 +1,21 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1> Weather App</h1>
+
+      <footer>
+        Project created by Andree Miclaus
+        <a
+          href="https://github.com/andreeamiclaus/react-final-project"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Open Source
+        </a>
+      </footer>
+    </div>
+  );
 }
