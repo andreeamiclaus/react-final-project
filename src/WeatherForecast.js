66b77a40f57fcc6ee092081a38b95ef0b810104a
andreeamiclaus <amiclaus.a@gmail.com>
2022-11-15 19:33:40 +0100

    Added Forecast for 5 day
    used loop and map function to iterate through the array

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -19,9 +19,16 @@ export default function WeatherForecast(props) {
     return (
       <div className="WeatherForecast">
         <div className="row">
-          <div className="col">
-            <WeatherForecastDay data={forecast[0]} />
-          </div>
+          {forecast.map(function (dailyForecast, index) {
+            if (index < 6) {
+              // to only show 5 days
+              return (
+                <div className="col" key={index}>
+                  <WeatherForecastDay data={dailyForecast} />
+                </div>
+              );
+            }
+          })}
         </div>
       </div>
     );
